c599540ecd24073fbd48c3bb57820e6c75df9ef3
binattimattia <binattimattia@gmail.com>
2025-02-17 15:49:25 +0100

    si

--- quadrati_magici.py
+++ quadrati_magici.py
@@ -6,17 +6,15 @@ matrice = []
 def genera_matrice(n) -> list:
     """Genera una matrice n x n con numeri casuali."""
     lista_numeri = []
-
+    
     for i in range(1, n + 1):
         lista_numeri.append(i)
-
-    lista_numeri_tolti = lista_numeri
 
     for riga in range(n):
         matrice_interna = []
         for colonna in range(n):
-            numerocasuale = random.choice(lista_numeri_tolti)
-            lista_numeri_tolti.remove(numerocasuale)
+            numerocasuale = random.choice(lista_numeri)
+            lista_numeri.remove(numerocasuale)
             matrice_interna.append(numerocasuale)
         matrice.append(matrice_interna)
     return matrice
